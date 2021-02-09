0a032ede4bf8a5ac38e5c4265661aa3686121374
gstephan30 <gstephan30@gmail.com>
2021-02-09 11:15:26 +0100

    finished unit tests

--- subreddit.R
+++ subreddit.R
@@ -52,8 +52,8 @@ kotw_entries_wide <- df %>%
 
 
 all_kotw <- kotw_entries_wide %>% 
-  mutate(reddit_url = glue::glue("https://www.reddit.com/{subreddit_name_prefixed}/comments/{id}")) %>% 
-  select(reddit_url)
+  mutate(reddit_url_json = glue::glue("https://www.reddit.com/{subreddit_name_prefixed}/comments/{id}")) %>% 
+  select(reddit_url_json)
 
 
 get_post <- function(json_tibble){
@@ -100,7 +100,19 @@ get_replies <- function(comment_tibble){
 subreddit <- function(json_url){
   
   #json_url <- "https://www.reddit.com/r/dominion/comments/4703zy.json"
+  #json_url <- "https://www.reddit.com/r/dominion/comments/4703zy/kotw_221_conspirator_courtyard_crossroads_great/d0f7luu.json"
+  #json_url <- "https://www.reddit.com/r/dominion/comments/4ac0xg"
+  #json_url <- "https://www.reddit.com/r/dominion/comments/3wqx3o"
+  
   print(json_url)
+  
+  if (str_detect(json_url, "\\.json$")) {
+    json_url <- json_url
+  } else {
+    json_url <- paste0(json_url, ".json")
+  }
+  
+  
   
   df_json <- tibble(
     json = jsonlite::read_json(json_url)
@@ -123,6 +135,7 @@ subreddit <- function(json_url){
     
     # check if replies exists
     run <- 1
+    #run <- 9
     ind <- 0
     depth_run <- run + ind
     df_reply <- NULL
@@ -136,54 +149,80 @@ subreddit <- function(json_url){
     }
     
     while (!is.na(df_reply[depth_run])) {
+    #while (length(df_reply) == depth_run) {
       print(paste0("Reading relies depth: ", depth_run))
+      #print(paste0("Reading relies depth: run ", run))
+      
       
       if (!"replies" %in% names(df_reply[[depth_run]])) {
         df_reply[[depth_run]] <- df_reply[[depth_run]] %>% 
           mutate(replies = as.list(NA))
       }
       
-      depth_run <- depth_run + 1
-      run <- run + 1
-      #print(run)
-      
-      df_reply[[depth_run]] <- df_reply[[depth_run-1]] %>% 
-        get_replies() %>% 
-        mutate(depth = depth_run)
-      
-      if ("replies" %in% names(df_reply[[depth_run]])) {
-        df_reply[[depth_run]] <- df_reply[[depth_run]] %>% 
-          mutate(replies = as.list(replies))
-      }
-      
-      if (run == 11) {
-        print(df_reply[[depth_run-2]]$id)
+      if (run == 10) {
+        print(df_reply[[depth_run-1]]$id)
         
-        run <- 1
-        ind <- ind + 10
-        
-        depth_url <- paste0("https://www.reddit.com", post_url, df_reply[[depth_run-2]]$id, ".json")
+        depth_url <- paste0("https://www.reddit.com", post_url, df_reply[[depth_run-1]]$id, ".json")
         print(depth_url)
         
-        df_reply[[depth_run]] <- tibble(
-          json = read_json("https://www.reddit.com/r/dominion/comments/4703zy/kotw_221_conspirator_courtyard_crossroads_great/d0f7luu.json")
-        ) %>% 
-          get_comments() %>% 
-          get_replies() %>% 
-          mutate(depth = ind + depth)
+        depp_rep <- NULL
+        for (i in seq_along(depth_url)) {
+          #i <- 1
+          depp_rep[[i]] <- tibble(
+            json = read_json(depth_url[i])
+          ) %>% 
+            get_comments() %>% 
+            get_replies() 
           
+          if ("replies" %in% names(depp_rep[[i]])) {
+            depp_rep[[i]] <- depp_rep[[i]] %>% 
+              mutate(replies = as.list(replies))
+          }
+          
+          if (is.na(depp_rep[i])) {
+            depp_rep[[i]] <- tibble()
+          }
+          
+        }
+        df_reply[[depth_run]] <- bind_rows(depp_rep)
+        
+        
+        if (!is.na(df_reply[depth_run])) {
+          df_reply[[depth_run]] <- df_reply[[depth_run]] %>% 
+            mutate(depth = depth_run)
+        }
+        
+        run <- 1
+        
+        ind <- ind + 10 - 1
+        
         depth_run <- run + ind
         
+      } else {
+        
+        depth_run <- depth_run + 1
+        run <- run + 1
+        #print(run)
+        
+        df_reply[[depth_run]] <- df_reply[[depth_run-1]] %>% 
+          get_replies() 
+        
+        if (!is.na(df_reply[depth_run])) {
+          df_reply[[depth_run]] <- df_reply[[depth_run]] %>% 
+            mutate(depth = depth_run)
+        }
+       
+        if ("replies" %in% names(df_reply[[depth_run]])) {
+          df_reply[[depth_run]] <- df_reply[[depth_run]] %>% 
+            mutate(replies = as.list(replies))
+        }
       }
       
       #print(length(df_reply))
     }
     
-
-    
-    #print("binding")
     df <- df_post_comm %>% 
-      bind_rows(df_reply[-run]) %>%
+      bind_rows(df_reply[-depth_run]) %>%
       mutate_all(as.character)
     
   } else {
@@ -191,35 +230,32 @@ subreddit <- function(json_url){
       mutate_all(as.character)
   }
   
-  
+  df <- df %>% 
+    mutate(reddit_id = str_sub(json_url, -11, -6))
   
   return(df)
 }
 
-subreddit("https://www.reddit.com/r/dominion/comments/3mo1pv.json")
-subreddit("https://www.reddit.com/r/dominion/comments/4703zy.json")
-
 all_kotw_reddits <- map_df(pull(all_kotw), subreddit)
+
 
 clean_subreddit <- function(subreddit) {
   clean_post <- subreddit %>% 
     filter(children_kind == "t3") %>% 
-    select(subreddit, subreddit_id, subreddit_subscribers, id, title, text = selftext, pwls, wls, ups, downs, score, 
+    select(reddit_id, subreddit, subreddit_id, subreddit_subscribers, id, title, text = selftext, pwls, wls, ups, downs, score, 
            name, upvote_ratio, edited, created, created_utc, permalink, 
-           total_awards_received, author_fullname, author, num_comments) %>% 
-    mutate(reddit_id = id)
+           total_awards_received, author_fullname, author, num_comments)
   
   clean_replies <- subreddit %>% 
     filter(children_kind == "t1") %>% 
-    select(subreddit, subreddit_id, id, text = body, ups, downs, score, name, depth, controversiality,
+    select(reddit_id, subreddit, subreddit_id, id, text = body, ups, downs, score, name, depth, controversiality,
            name, parent_id, link_id, edited, created, created_utc, author, author_fullname,
            permalink)
   
   df <- bind_rows(
     clean_post, 
     clean_replies
-  ) %>% 
-    fill(reddit_id, .direction = "down")
+  ) 
   
   return(df)
 }
