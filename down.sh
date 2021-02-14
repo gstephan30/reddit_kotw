b6055f42f6826e0a11d6b3a0f147f72fed42c56a
stephan <gstephan30@gmail.com>
2021-02-14 13:36:14 +0000

    20210214 data push

---
+++ down.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# run script
+cd reddit_kotw
+Rscript subreddit.R
+
+# Nightly push to github
+
+# Set some variables
+depdir=/home/pi/reddit_kotw
+jetzt=`date +%Y%m%d`
+
+# Now add any changes
+git add .
+# Now commit
+git commit -m "$jetzt data push"
+git push -u origin master
+
+cd ..
