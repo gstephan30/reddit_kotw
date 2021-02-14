e065be80b7541aa243f47d56e8aecc22dc329ac3
Stephan Glöckner <gstephan30@gmail.com>
2021-02-14 20:21:08 +0100

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# run script
-cd reddit_kotw
-Rscript subreddit.R
-
 # Nightly push to github
-
 # Set some variables
 depdir=/home/pi/reddit_kotw
 jetzt=`date +%Y%m%d`
 
 # update rep
+cd reddit_kotw
 git pull
+
+# run script
+Rscript subreddit.R
 
 # Now add any changes
 git add .
