1a12a5427c42005f00f29796f16245fc527a83e1
gstephan30 <gstephan30@gmail.com>
2021-02-22 09:20:04 +0100

    added pairwise correlation post

--- R/reddit_comments.R
+++ R/reddit_comments.R
@@ -89,20 +89,45 @@ vertices <- stemmed_tokens %>%
 
 library(ggraph)
 library(igraph)
-library(ggnewscale)
+library(showtext)
+font_add_google("Schoolbell", "bell")
+showtext_auto()
+#library(ggnewscale)
 set.seed(2021)
-pcor %>% 
+g_cor <- pcor %>% 
   graph_from_data_frame(vertices = vertices) %>% 
   ggraph("fr") +
   
   geom_edge_link(aes(color = correlation), edge_width = 1.2) +
-  scale_color_gradient2(low = "blue", high = "red", midpoint = 0.6) +
-  new_scale_color() +
+  scale_edge_color_gradient2(low = "#72bcd4", high = "red", midpoint = 0.6) +
+  #new_scale_color() +
  
   geom_node_point(size = 3.5, color = "black") +
   geom_node_point(aes(color = n), size = 3) +
   scale_color_viridis_c() +
-  geom_node_text(aes(label = name), repel = TRUE) +
-  theme_void() 
-  
+  geom_node_text(aes(label = name), repel = TRUE, size = 12, family = "bell") +
+  theme_void(base_family = "bell", base_size = 24) +
+  labs(title = 'Correlation of words appeared in reddits Kingdom of the week ("KOTW") posts',
+       subtitle = paste0("Words are corrected and stemmed, date: ", format(heute, "%b %d, %Y"), "\n",
+                         "Words filterd with p (rho) >0.25"),
+       color = "Count word stem appeared",
+       edge_color = "Word pair correlation"
+       ) +
+  theme(legend.position = "bottom",
+        plot.subtitle = element_text(lineheight = 0.5)) +
+  guides(color = guide_colorbar(title.position = "top"),
+         edge_color = guide_edge_colorbar(title.position = "top"))
+
+ggsave(g_cor, filename = "gen_figs/comment_cors.jpg")
+
+tm_link <- "https://new.reddit.com/search/?q=author%3Aavocadro%20title%3AKotW&sort=new"
+tm_hashs <- " #dominion #boardgame #boardgamegeek #riograndegames #boardgames #deckbuilder #dominionboardgame "
+tm_message <- paste0("Text-mining analysis of Dominion cards from reddits Kingdom of the week comments ",
+                     tm_link,
+                     tm_hashs,
+                     "#rstats")
+post_tweet(
+  status = tm_message,
+  media = "gen_figs/comment_cors.jpg"
+)  
   
